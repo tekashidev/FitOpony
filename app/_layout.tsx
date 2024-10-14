302ebb54c595deba06d58d1eeaf39fb33f8f2f2a
tekashi <szymon.programmer@gmail.com>
2024-10-14 22:53:33 +0200

    Init

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
+export default function RootLayout() {
+  return (
+    <GluestackUIProvider mode="light"><Stack>
+        <Stack.Screen name="index" />
+      </Stack></GluestackUIProvider>
+  );
+}
